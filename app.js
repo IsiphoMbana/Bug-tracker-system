0507962eb0035ee637bddf009a5b040d4ba9fab5
mkhabeleshihluke-bit <mkhabeleshihluke@gmail.com>
2026-04-22 15:42:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
-//here we created the projects(basically the apps and the websites) that will be assigned to the issues
-let projects = 
-[
-  { id: 1, name: "Campus Laundry Booking System" },
-  { id: 2, name: "Gym Membership Management App" },
-  { id: 3, name: "Food Truck Ordering Platform" }
-]
-//this is creating projects using a function 
-function createProject(name) 
-{
-    let newProject = {
-        id: projects.length + 1,
-        name
-    };
-
-    projects.push(newProject);
-    saveData();
-}
 
